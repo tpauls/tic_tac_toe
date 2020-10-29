31ad1fc02324b67adfd4dfef79f0ddb5375a5ec5
Justleecho <MrEricLe@gmail.com>
2020-10-28 21:11:37 -0700

    Logic Updated

--- Eric/game.py
+++ Eric/game.py
@@ -15,14 +15,14 @@ def board():
     print("\t  {}  |  {}  |  {}".format(gameboard[3], gameboard[4], gameboard[5]))
     print('\t_____|_____|_____')
     print("\t     |     |") 
-    print("\t  {}  |  {}  |  {}".gameboard(values[6], gameboard[7], gameboard[8]))
+    print("\t  {}  |  {}  |  {}".format(gameboard[6], gameboard[7], gameboard[8]))
     print("\t     |     |")
     print("\n")
 
 
 player1n = input('What is your name Player 1: ')
 player1t = input('Do you wnat to use the X or O: ')
-player2n = input('What is your name Player 1: ') 
+player2n = input('What is your name Player 2: ') 
 
 if player1t == 'X':
     player2t = 'O'
@@ -33,7 +33,8 @@ gameboard = [' ' for x in range(9)]
 gameboard[0] = 'X'
 gameboard[1] = 'X'
 board()
+
 player1= Player(player1n, player1t)
 player2= Player(player2n, player2t)
 
-
+print('Upload')
