4933d869a416a82de43a0728d3c67461c690aa80
Justleecho <MrEricLe@gmail.com>
2020-10-29 18:51:05 -0700

    Logic Updated

--- Eric/game.py
+++ Eric/game.py
@@ -2,6 +2,9 @@ class Player :
     def __init__(self, name , token):
         self.name = name
         self.token = token
+    def display (self):
+        s ="It Your Turn " + str(self.name)
+        return s
 # class Game(Player):
 #     # def __init__(self, name, token, board):
 #     #     super().__init__(length, width)
@@ -19,22 +22,47 @@ def board():
     print("\t     |     |")
     print("\n")
 
+def XO():
+    while True:
+     xo = input(f'Do you want to be X or O: ')
+     xo.capitalize()
+     if  xo == 'X' or xo == 'O':   
+          return xo
+     else:
+          print('\nInvaid input X or O!')
+
+
 
 player1n = input('What is your name Player 1: ')
-player1t = input('Do you wnat to use the X or O: ')
+player1t = XO()
 player2n = input('What is your name Player 2: ') 
 
 if player1t == 'X':
     player2t = 'O'
 else:
-    player2t == 'X'
+    player2t = 'X'
 
-gameboard = [' ' for x in range(9)]
-gameboard[0] = 'X'
-gameboard[1] = 'X'
-board()
+gameboard = [' ' for x in range(9)] # Clear Board
+# gameboard[0] = 'X'
+# gameboard[1] = 'X'
+# board()
 
 player1= Player(player1n, player1t)
 player2= Player(player2n, player2t)
 
-print('Upload')
+turn = 1
+while turn != 10:
+    if (turn % 2) != 0:
+        print(player1.display())
+        input("your Move :")
+        print(player1.token)
+        turn +=1
+    
+    else:
+        print(player2.display())
+        input("your Move :")
+        print(player2.token)
+        turn +=1
+if turn == 10:
+    print('Draw')
+
